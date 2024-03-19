29f7843e0db17db5385ca57e9728cca5cd785177
yurokbrat <yurokbrat@yandex.ru>
2024-03-19 17:35:07 +0300

    Исправил ошибку, при которой во время создании медиа происходила ошибка и улучшил отображение времени создания ссылки

--- kino/utils/check_urls_to_quality.py
+++ kino/utils/check_urls_to_quality.py
@@ -1,14 +1,12 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 from kino.video.models import VideoQuality
 
 
 def urls_to_quality(instance, quality, path):
-    url = VideoQuality.objects.get(media=instance, quality=quality)
-    if url.video_url is None:
-        VideoQuality.objects.create(media=instance,
-                                    quality=quality,
-                                    video_url=path)
-    else:
-        url.media = instance
-        url.quality = quality
+    try:
+        url = VideoQuality.objects.get(media=instance, quality=quality)
         url.video_url = path
         url.save()
+    except ObjectDoesNotExist:
+        VideoQuality.objects.create(media=instance, quality=quality, video_url=path)
