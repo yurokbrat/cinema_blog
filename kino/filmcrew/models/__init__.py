05a5fea4dd1374d63f25ae3d9234cc2acbd16d4f
yuriy.belotserkovskiy <yuriy.belotserkovskiy@redcollar.ru>
2024-10-14 18:57:42 +0300

    fix pre-commit error

--- kino/filmcrew/models/__init__.py
+++ kino/filmcrew/models/__init__.py
@@ -1,4 +1,4 @@
 from kino.filmcrew.models.film_crew import FilmCrew
 from kino.filmcrew.models.photo_person import PhotoPerson
 
-_all_ = ("FilmCrew", "PhotoPerson")
+__all__ = ("FilmCrew", "PhotoPerson")
